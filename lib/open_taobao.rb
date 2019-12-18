b3fccb0d904141d1fae41d29c53d161e5b22bd47
zhangdabei <keepyouliking@gmail.com>
2019-12-18 10:47:00 +0800

    去掉引用

--- lib/open_taobao.rb
+++ lib/open_taobao.rb
@@ -4,7 +4,7 @@ require 'multi_json'
 require 'yaml'
 
 require 'open_taobao/version'
-require 'open_taobao/hash_ext'
+
 require 'open_taobao/open_taobao'
 
 require 'open_taobao/railtie' if defined? Rails
